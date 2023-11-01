a14eb9cb7fd1d72d51d6da17d463007ae0677516
Sebastian Martinez <me@sebastinez.dev>
2023-11-01 12:07:37 +0100

    Ok not lets try this
    Signed-off-by: Sebastian Martinez <me@sebastinez.dev>

--- publish.sh
+++ publish.sh
@@ -45,7 +45,7 @@ do
   ((COUNTER+=1))
   # (( COUNTER++ ))
   pushd "$item"
-  # echo "🚀 Publishing ${item}... (${COUNTER} of ${#packages[@]})"
+  echo "🚀 Publishing ${item}... (${COUNTER} of ${#packages[@]})"
   # echo "🚀 Publishing ${item}..."
   cat package.json | jq -r '. | .name + " " +.version'
   # npm publish --access=public
